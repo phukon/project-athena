d19ff24f49c54534f81cadbc6f872994514b1ee9
phukon <rikiphukon16@gmail.com>
2023-02-09 13:30:26 +0530

    fixed code

--- script1.js
+++ script1.js
@@ -98,102 +98,18 @@ function query_mech7() {
 // this is the CONTROLLER (selects queries based on the type of webpage)
 // please review if you buy domain names!!
 const currentUrl = window.location.href;
+const pages = {
+  "etc": [1, 2, 3, 4, 5, 6, 7, 8],
+  "mecha": [1, 2, 3, 4, 5, 6, 7, 8],
+  "civil": [1, 2, 3, 4, 5, 6, 7, 8]
+};
 
-switch(true) {
-  case currentUrl.includes("http://127.0.0.1:5500/etc-8.html"):
-    query_etc8();
-    break;
-  case currentUrl.includes("https://phukon.github.io/project-athena/etc-7.html"):
-    query_etc7();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/etc-6.html"):
-    query_etc6();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/etc-5.html"):
-    query_etc5();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/etc-4.html"):
-    query_etc4();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/etc-3.html"):
-    query_etc3();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/etc-2.html"):
-    query_etc2();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/etc-1.html"):
-    query_etc1();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/mecha-8.html"):
-    query_mecha8();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/mecha-7.html"):
-    query_mecha7();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/mecha-6.html"):
-    query_mecha6();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/mecha-5.html"):
-    query_mecha5();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/mecha-4.html"):
-    query_mecha4();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/mecha-3.html"):
-    query_mecha3();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/mecha-2.html"):
-    query_mecha2();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/mecha-1.html"):
-    query_mecha1();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/civil-8.html"):
-    query_civil8();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/civil-7.html"):
-    query_civil7();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/civil-6.html"):
-    query_civil6();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/civil-5.html"):
-    query_civil5();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/civil-4.html"):
-    query_civil4();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/civil-3.html"):
-    query_civil3();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/civil-2.html"):
-    query_civil2();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/civil-2.html"):
-    query_civil1();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/pei-8.html"):
-    query_pei8();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/pei-7.html"):
-    query_pei7();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/pei-6.html"):
-    query_pei6();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/pei-5.html"):
-    query_pei5();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/pei-4.html"):
-    query_pei4();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/pei-3.html"):
-    query_pei3();
-    break;
-  case currentUrl.includes("http://127.0.0.1:5500/pei-2.html"):
-    query_pei2();
-    break;
-  default:
-    query_pei1();
-    break;
+for (const [prefix, suffixes] of Object.entries(pages)) {
+  for (const suffix of suffixes) {
+    const url = `https://phukon.github.io/project-athena/${prefix}-${suffix}.html`;
+    if (currentUrl.includes(url)) {
+      window[`query_${prefix}${suffix}`]();
+      break;
+    }
+  }
 }
