67526a24912aec8addf9b68d5b00ebd81d472b6d
phukon <rikiphukon16@gmail.com>
2023-02-14 15:11:05 +0530

    .minor change

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@
     // Check if the cookie has been set
     if (!document.cookie.replace(/(?:(?:^|.*;\s*)popupShown\s*\=\s*([^;]*).*$)|^.*$/, "$1")) {
       // Set the cookie
-      document.cookie = "popupShown=true; expires=Fri, 10 Feb 2023 23:59:59 GMT; path=/";
+      document.cookie = "popupShown=true; expires=Thu, 16 Feb 2023 23:59:59 GMT; path=/";
       // Show the popup
       alert("The site is under construction🚧🏗 with some features unavailable at this time. This is a one-man project and all support is appreciated. I will be open-sourcing the code under the MIT License. The resources are free for all, FOREVER!✨ Thanks");
     }
