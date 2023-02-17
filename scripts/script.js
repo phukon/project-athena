88a4fa03d76d129aa782906faa4f06fcd3a63c43
phukon <rikiphukon16@gmail.com>
2023-02-18 02:42:15 +0530

    popup module

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,5 @@
-/* -- Carousel Navigation -- */
-
-function showPopup() {
-    // Check if the cookie has been set
-    if (!document.cookie.replace(/(?:(?:^|.*;\s*)popupShown\s*\=\s*([^;]*).*$)|^.*$/, "$1")) {
-      // Set the cookie
-      document.cookie = "popupShown=true; expires=tue, 14 Feb 2023 23:59:59 GMT; path=/";
-      // Show the popup
-      alert("The site is under construction🚧🏗 with some features unavailable at this time. This is a one-man project and all support is appreciated. I will be open-sourcing the code under the MIT License. The resources are free for all, FOREVER!✨ Thanks");
-    }
-  }
-
+import { showPopup } from "../popup.js"
+showPopup();
 
 let activeIndex = 0;
 
